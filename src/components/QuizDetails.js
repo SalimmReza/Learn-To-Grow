b5e4895c0ebe073d9343842ac5af2de14bd4bf3e
SalimmReza <salimnuaa@gmail.com>
2022-10-11 14:37:33 +0600

    correct and wrong ans done

--- src/components/QuizDetails.js
+++ src/components/QuizDetails.js
@@ -1,37 +1,83 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const QuizDetails = ({ questions }) => {
-    console.log(questions)
-    const index = questions.indexof
-    // console.log(questions.indexof)
+    // console.log(questions.correctAnswer)
+
+
+    const quesHandleClick = (e) => {
+        const newCart = e.target.innerText;
+        if (newCart !== questions.correctAnswer) {
+            console.log(questions.correctAnswer.length, newCart.length)
+
+            const notify = () => {
+                // Set to 10sec
+                toast.warning('Danger', { autoClose: 10000 });
+                toast('Hello Geeks');
+
+            }
+        }
+        else {
+            alert('Great Job!')
+            console.log("c", questions.correctAnswer.length, newCart.length)
+        }
+
+    }
+
+
+
+    const eyeHandleClick = () => {
+        const newCart = questions.correctAnswer;
+        // console.log(newCart)
+
+    }
+
+    const spit = questions.question.split('<p>')
+    const a = spit.join("")
+    const spit2 = questions.question.split('</p>')
+    const b = spit.join("")
 
     return (
         <div>
             <div className='w-[50%] m-auto'>
 
 
-                <div className='h-[350px] w-full bg-white-500 shadow-lg rounded-lg mt-20 shadow-green-800 pt-5'>
-                    <h1>{questions.question}</h1>
+                <div className='h-[400px] w-full bg-white-500 shadow-lg rounded-lg mt-20 shadow-green-800 pt-2'>
+
+                    <h1 onClick={eyeHandleClick, notify} className='flex justify-end px-2'>  <FontAwesomeIcon className='flex justify-end cursor-pointer' icon={faEye}></FontAwesomeIcon></h1>
+
+
+                    <h1>{a}</h1>
+
                     <div className='flex my-10'>
                         <div className='flex items-center p-2 h-[100px] w-[50%] mx-5 border-2 border-green-200 rounded-lg'>
-                            <h1>{questions.options[0]}</h1>
+
+                            <h1 onClick={(e) => quesHandleClick(e)}>{questions.options[0]}</h1>
                         </div>
                         <div className=' flex items-center p-2 mx-5 h-[100px] w-[50%] border-2 border-green-200 rounded-lg '>
-                            <h1>{questions.options[1]}</h1>
+
+                            <h1 onClick={(e) => quesHandleClick(e)}>{questions.options[1]}</h1>
                         </div>
 
                     </div>
 
                     <div className='flex'>
                         <div className='p-2 flex items-center h-[100px] w-[50%] mx-5 border-2 border-green-200 rounded-lg'>
-                            <h1>{questions.options[2]}</h1>
+
+                            <h1 onClick={(e) => quesHandleClick(e)}>{questions.options[2]}</h1>
                         </div>
                         <div className=' p-2 flex items-center mx-5 h-[100px] w-[50%] border-2 border-green-200 rounded-lg '>
-                            <h1>{questions.options[3]}</h1>
+
+                            <h1 onClick={(e) => quesHandleClick(e)}>{questions.options[3]}</h1>
                         </div>
 
                     </div>
 
+                    <ToastContainer />
                 </div>
             </div>
         </div>
--- src/components/StatisticDetails.js
+++ src/components/StatisticDetails.js
@@ -1,46 +1,74 @@
-import React, { useState } from 'react';
-import {
-    BarChart,
-    Bar,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend
-} from "recharts";
+import React, { useEffect, useState } from 'react';
+import Chart from 'react-apexcharts';
 
 const StatisticDetails = ({ subject }) => {
     const { name, id, logo, total } = subject
-    // console.log(subject)
+    console.log(subject)
 
 
-    const data = [
-        {
-            name: name,
-            uv: total
-        },
+    const [sData, setSdata] = useState([]);
 
-    ];
+
+    const newvalue = [];
+
+    for (let i = 0; i < subject.length; i++) {
+        newvalue.push([{ name: subject[i].name, data: subject[i].total }]);
+
+    }
+
+
+    // for (const d in resData) {
+    //     newvalue.push([{ name: d.name, data: d.total }]);
+    // }
+    setSdata(newvalue);
+
+
+
+
     return (
-        <BarChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
+        <React.Fragment>
+            <div className='container-fluid mt-3 mb-3'>
 
-            <Bar dataKey="uv" fill="#82ca9d" />
-        </BarChart>
+                <Chart type='line'
+                    width={1450}
+                    height={350}
+                    series={sData}
+                    options={{
+                        title: { text: "Product sell in 2021" },
+                        xaxis: {
+                            title: { text: "Product Sell in Months" },
+                            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                        },
+                        yaxis: {
+                            title: { text: "Product in K" }
+                        }
+
+                    }}
+                >
+                </Chart>
+
+            </div>
+
+        </React.Fragment >
+        //     <BarChart
+        //     //     width={500}
+        //     //     height={300}
+        //     //     data={data}
+        //     //     margin={{
+        //     //         top: 5,
+        //     //         right: 30,
+        //     //         left: 20,
+        //     //         bottom: 5
+        //     //     }}
+        //     // >
+        //     //     <CartesianGrid strokeDasharray="3 3" />
+        //     //     <XAxis dataKey="name" />
+        //     //     <YAxis />
+        //     //     <Tooltip />
+        //     //     <Legend />
+
+        //     //     <Bar dataKey="uv" fill="#82ca9d" />
+        //     </BarChart >
     );
 };
 
