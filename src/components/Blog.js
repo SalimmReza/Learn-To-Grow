fd5939e8b7a168b4ab93352d6db3f8f9ad1fd6f4
SalimmReza <salimnuaa@gmail.com>
2022-10-11 16:47:12 +0600

    blog page done

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Subject from './Subject';
+
 
 const Blog = () => {
-    const subjects = useLoaderData();
+
     return (
-        <div>
-            {
-                subjects.map(subject => <Subject
-                    key={subject.id}
-                    subject={subject}
-                ></Subject>)
-            }
+        <div className='w-[50%] m-auto mt-20'>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    Purpose of React Router
+                </div>
+                <div className="collapse-content">
+                    <p>React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.</p>
+                </div>
+            </div>
+
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    How does Context Api work
+                </div>
+                <div className="collapse-content">
+                    <p>React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state. More information can be found on React's documentation page</p>
+                </div>
+            </div>
+
+
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    useRef()
+                </div>
+                <div className="collapse-content">
+                    <p>The useRef Hook allows you to persist values between renders.
+
+                        It can be used to store a mutable value that does not cause a re-render when updated.
+
+                        It can be used to access a DOM element directly. <br />
+                        If we tried to count how many times our application renders using the useState Hook, we would be caught in an infinite loop since this Hook itself causes a re-render. <br />
+                        useRef() only returns one item. It returns an Object called current.
+
+                        When we initialize useRef we set the initial value: useRef(0).
+
+
+
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
